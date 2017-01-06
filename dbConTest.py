33483900544858e3743080a320481dd04a70fa39
Mingun Pak <kaienmg@gmail.com>
2017-01-05 18:09:14 -0800

    DB Connection Test, good

--- dbConTest.py
+++ dbConTest.py
@@ -1,6 +1,21 @@
 import mysql.connector
+from mysql.connector import errorcode
 
-cnx = mysql.connector.connect(user='root',
-                              password='root',
-                              host='127.0.0.1',
-                              database='test')
+config = {
+  'user': 'root',
+  'password': 'root',
+  'host': '127.0.0.1',
+  'database': 'test',
+}
+
+try:
+    cnx = mysql.connector.connect(**config)
+except mysql.connector.Error as err:
+    if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+        print("Something is wrong with your user name or password")
+    elif err.errno == errorcode.ER_BAD_DB_ERROR:
+        print("Database does not exist")
+    else:
+        print(err)
+else:
+    cnx.close()
